9bcba7f6ee92342d9db86e038faf6fed91175c44
David <davidmunozpellicer@gmail.com>
2020-05-13 16:38:39 +0200

    Initial commit

---
+++ WorleyNoise.py
@@ -0,0 +1,25 @@
+from PIL import Image
+from random import randint
+import numpy as np
+
+WIDTH, HEIGHT = 400, 400
+n = 0
+points = []
+nPoints = 20
+points = [np.array([randint(0,WIDTH),randint(0,HEIGHT)]) for i in range(nPoints)]
+
+img = Image.new('RGB',(WIDTH,HEIGHT))
+
+pixels = img.load()
+
+for x in range(WIDTH):
+	for y in range(HEIGHT):
+		d = np.zeros(len(points))
+		for i,point in enumerate(points):
+			d[i] = ((point[0]-x)**2+(point[1]-y)**2)**0.5
+		d.sort()
+		c = int(d[n])
+		pixels[x,y] = (c,c,c)
+
+img.show()
+img.save('Example.jpg','JPEG')
